0722e8b9a02b336e43ae303555531480eb28e007
Giorgi Kharebava <106274061+kharebavagiorgi@users.noreply.github.com>
2025-11-04 00:03:56 +0400

    Update Library.java

--- src/main/java/com/solvd/library/model/Library.java
+++ src/main/java/com/solvd/library/model/Library.java
@@ -14,13 +14,21 @@ public class Library {
     @XmlAttribute
     private String name;
 
-    @XmlElementWrapper(name = "books", namespace = "http://www.library.com/schema")
-    @XmlElement(name = "book", namespace = "http://www.library.com/schema")
+    @XmlElementWrapper(name = "books")
+    @XmlElement(name = "book")
     private List<Book> books = new ArrayList<>();
 
-    @XmlElementWrapper(name = "users", namespace = "http://www.library.com/schema")
-    @XmlElement(name = "user", namespace = "http://www.library.com/schema")
+    @XmlElementWrapper(name = "users")
+    @XmlElement(name = "user")
     private List<User> users = new ArrayList<>();
+
+    @XmlElementWrapper(name = "departments")
+    @XmlElement(name = "department")
+    private List<Department> departments = new ArrayList<>();
+
+    @XmlElementWrapper(name = "employees")
+    @XmlElement(name = "employee")
+    private List<Employee> employees = new ArrayList<>();
 
     public Library() {
     }
@@ -33,4 +41,10 @@ public class Library {
 
     public List<User> getUsers() { return users; }
     public void setUsers(List<User> users) { this.users = users; }
+
+    public List<Department> getDepartments() { return departments; }
+    public void setDepartments(List<Department> departments) { this.departments = departments; }
+
+    public List<Employee> getEmployees() { return employees; }
+    public void setEmployees(List<Employee> employees) { this.employees = employees; }
 }
