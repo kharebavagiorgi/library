9e12e29fe42568b638e52b92adb8dbdd6bba456b
Giorgi Kharebava <106274061+kharebavagiorgi@users.noreply.github.com>
2025-11-07 14:54:53 +0400

    Update XmlValidator.java

--- src/main/java/com/solvd/library/parser/XmlValidator.java
+++ src/main/java/com/solvd/library/parser/XmlValidator.java
@@ -1,5 +1,8 @@
 package com.solvd.library.parser;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.xml.XMLConstants;
 import javax.xml.transform.stream.StreamSource;
 import javax.xml.validation.Schema;
@@ -9,17 +12,19 @@ import java.io.File;
 
 public class XmlValidator {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(XmlValidator.class);
+
     public static boolean validate(String xmlFilePath, String xsdFilePath) {
         try {
             SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
             Schema schema = factory.newSchema(new File(xsdFilePath));
             Validator validator = schema.newValidator();
-            System.out.println("Attempting to validate " + xmlFilePath + "...");
+            LOGGER.debug("Attempting to validate {}...", xmlFilePath);
             validator.validate(new StreamSource(new File(xmlFilePath)));
-            System.out.println("✅ XML is valid against XSD.");
+            LOGGER.info("XML is valid against XSD.");
             return true;
         } catch (Exception e) {
-            System.err.println("❌ XML is NOT valid. Reason: " + e.getMessage());
+            LOGGER.error("XML is NOT valid. Reason: " + e.getMessage());
             return false;
         }
     }
