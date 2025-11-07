877549e3dcc914a5623d6e6391edc19681457f76
Giorgi Kharebava <106274061+kharebavagiorgi@users.noreply.github.com>
2025-11-07 14:59:35 +0400

    Update Book.java

--- src/main/java/com/solvd/library/model/Book.java
+++ src/main/java/com/solvd/library/model/Book.java
@@ -1,33 +1,61 @@
 package com.solvd.library.model;
 
 import java.time.LocalDate;
-import jakarta.xml.bind.annotation.*;
+import java.util.ArrayList;
+import java.util.List;
 
-@XmlType(propOrder = {"isbn", "title", "publicationDate", "pageCount", "genre"})
+import com.solvd.library.localdateadapter.LocalDateAdapter;
+import jakarta.xml.bind.annotation.*;
+import jakarta.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+
+@XmlType(propOrder = {"id", "isbn", "title", "publicationDate", "pageCount", "genre", "reviews"}) //
+@XmlAccessorType(XmlAccessType.PROPERTY)
 public class Book {
+    private Long id;
     private String isbn;
     private String title;
     private LocalDate publicationDate;
     private int pageCount;
     private Genre genre;
 
-    @XmlAttribute
+
+    private static final String LIBRARY_NAMESPACE = "http://www.library.com/schema";
+    private List<Review> reviews = new ArrayList<>();
+
+    public Book() {}
+
+    @XmlElement(namespace = LIBRARY_NAMESPACE)
     public String getIsbn() { return isbn; }
     public void setIsbn(String isbn) { this.isbn = isbn; }
 
-    @XmlElement
+    @XmlElement(namespace = LIBRARY_NAMESPACE)
     public String getTitle() { return title; }
     public void setTitle(String title) { this.title = title; }
 
-    @XmlElement
+    @XmlElement(namespace = LIBRARY_NAMESPACE)
     public int getPageCount() { return pageCount; }
     public void setPageCount(int pageCount) { this.pageCount = pageCount; }
 
-    @XmlElement
+    @XmlElement(namespace = LIBRARY_NAMESPACE)
+    @XmlJavaTypeAdapter(LocalDateAdapter.class)
     public LocalDate getPublicationDate() { return publicationDate; }
     public void setPublicationDate(LocalDate publicationDate) { this.publicationDate = publicationDate; }
 
-    @XmlElement
+    @XmlElement(namespace = LIBRARY_NAMESPACE)
     public Genre getGenre() { return genre; }
     public void setGenre(Genre genre) { this.genre = genre; }
+
+    @XmlElementWrapper(name = "reviews", namespace = LIBRARY_NAMESPACE)
+    @XmlElement(name = "review", namespace = LIBRARY_NAMESPACE)
+    public List<Review> getReviews() {
+        return reviews;
+    }
+
+    @XmlElement(namespace = LIBRARY_NAMESPACE)
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public void setReviews(List<Review> reviews) {
+        this.reviews = reviews;
+    }
 }
