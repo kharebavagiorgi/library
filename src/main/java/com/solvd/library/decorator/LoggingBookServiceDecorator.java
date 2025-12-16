af5209f1ff8092a4479bd69ecad719c0caee2f01
Giorgi Kharebava <106274061+kharebavagiorgi@users.noreply.github.com>
2025-12-16 11:09:51 +0400

    Implementing createBookAndAuthor() method

--- src/main/java/com/solvd/library/decorator/LoggingBookServiceDecorator.java
+++ src/main/java/com/solvd/library/decorator/LoggingBookServiceDecorator.java
@@ -1,5 +1,6 @@
 package com.solvd.library.decorator;
 
+import com.solvd.library.domain.Author;
 import com.solvd.library.domain.Book;
 import com.solvd.library.service.BookService;
 import org.slf4j.Logger;
@@ -29,6 +30,20 @@ public class LoggingBookServiceDecorator implements BookService {
     }
 
     @Override
+    public Book createBookAndAuthor(Book book, Author author) {
+        LOGGER.info("DECORATOR LOG: Starting COMPLEX creation of book '{}' and author '{}'.",
+                book.getTitle(),
+                author != null ? author.getName() : "None");
+
+        Book createdBook = wrappee.createBookAndAuthor(book, author);
+
+        LOGGER.info("DECORATOR LOG: COMPLEX creation completed. New Book ID: {}", createdBook.getId());
+
+        return createdBook;
+    }
+    // -------------------------------------------------------------------
+
+    @Override
     public Optional<Book> findById(Long id) {
         LOGGER.info("DECORATOR LOG: Searching for book with ID: {}", id);
         Optional<Book> result = wrappee.findById(id);
@@ -46,12 +61,16 @@ public class LoggingBookServiceDecorator implements BookService {
 
     @Override
     public void update(Book entity) {
+        LOGGER.info("DECORATOR LOG: Attempting to update book with ID: {}", entity.getId());
         wrappee.update(entity);
+        LOGGER.info("DECORATOR LOG: Book ID {} updated successfully.", entity.getId());
     }
 
     @Override
     public void delete(Long id) {
+        LOGGER.warn("DECORATOR LOG: Attempting to delete book with ID: {}", id);
         wrappee.delete(id);
+        LOGGER.warn("DECORATOR LOG: Book ID {} deleted successfully.", id);
     }
 
     @Override
